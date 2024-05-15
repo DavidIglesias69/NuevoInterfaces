56441353674fd23b54b4a040fc94d275247cb95a
dic43 <dic43@PortatilDavid>
2024-05-15 20:40:32 +0200

    casa

---
+++ Interfaces/src/PinturasDB/AdministradorDB.java
@@ -0,0 +1,140 @@
+package PinturasDB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import PinturasInterface.Conexion;
+
+public class AdministradorDB {
+
+    public static double obtenerPrecio(String nombreProducto) throws SQLException {
+        Conexion conexion = new Conexion();
+        Connection con = conexion.abrirConsulta();
+        double precio = 0;
+        try {
+            String sql = "SELECT precio FROM productos WHERE nombre = ?";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, nombreProducto);
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                precio = rs.getDouble("precio");
+            }
+            rs.close();
+            pst.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        return precio;
+    }
+
+    public static double obtenerCantidad(String nombreProducto) throws SQLException {
+        Conexion conexion = new Conexion();
+        Connection con = conexion.abrirConsulta();
+        double cantidad = 0;
+        try {
+            String sql = "SELECT cantidad FROM productos WHERE nombre = ?";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, nombreProducto);
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                cantidad = rs.getDouble("cantidad");
+            }
+            rs.close();
+            pst.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        return cantidad;
+    }
+
+    public static void añadirProducto(String nombreProducto, int cantidad) throws SQLException {
+        Conexion conexion = new Conexion();
+        Connection con = conexion.abrirConsulta();
+        try {
+            String sql = "UPDATE productos SET cantidad = cantidad + ? WHERE nombre = ?";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setInt(1, cantidad);
+            pst.setString(2, nombreProducto);
+            pst.executeUpdate();
+            pst.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void eliminarProducto(String nombreProducto, int cantidad) throws SQLException {
+        Conexion conexion = new Conexion();
+        Connection con = conexion.abrirConsulta();
+        try {
+            String sql = "UPDATE productos SET cantidad = cantidad - ? WHERE nombre = ?";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setInt(1, cantidad);
+            pst.setString(2, nombreProducto);
+            pst.executeUpdate();
+            pst.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void actualizarPrecio(String nombreProducto, double nuevoPrecio) throws SQLException {
+        Conexion conexion = new Conexion();
+        Connection con = conexion.abrirConsulta();
+        try {
+            String sql = "UPDATE productos SET precio = ? WHERE nombre = ?";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setDouble(1, nuevoPrecio);
+            pst.setString(2, nombreProducto);
+            pst.executeUpdate();
+            pst.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+}
--- Interfaces/src/PinturasInterface/Administrador.java
+++ Interfaces/src/PinturasInterface/Administrador.java
@@ -1,34 +1,24 @@
 package PinturasInterface;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
-import PinturasDB.ProductoDB;
-import ProyectoPinturas.Usuario;
-import javax.swing.JLabel;
-import javax.swing.ImageIcon;
-import javax.swing.JComboBox;
-import java.awt.Font;
-import java.awt.Color;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JButton;
-import javax.swing.JSpinner;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
-import javax.swing.JOptionPane;
-import javax.swing.SpinnerNumberModel;
+import PinturasDB.AdministradorDB;
+import PinturasDB.ProductoDB;
+import ProyectoPinturas.Usuario;
 
 public class Administrador extends JFrame {
 
-    private static final long serialVersionUID = 1L;
     private JPanel despegableProductos;
-    private JLabel precioDB;  // Atributo de clase para precioDB
+    private JLabel precioDB;
     private JLabel cantidadDB;
+    private JTextField txtNuevoPrecio;
+    private JSpinner spinner;
 
     public Administrador(Usuario user) {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -39,61 +29,113 @@ public class Administrador extends JFrame {
         despegableProductos.setLayout(null);
 
         JComboBox<String> comboBox = new JComboBox<>();
-        comboBox.setModel(new DefaultComboBoxModel<>(new String[] {"", "Pintura", "Rodillo", "Papel", "Brocha", "Escalera", "Barniz", "Disolvente", "Plasticos", "Decapante", "Espatula"}));
+        comboBox.setModel(new DefaultComboBoxModel<>(new String[]{
+                "", "Pintura", "Rodillo", "Papel", "Brocha", "Escalera", "Barniz", "Disolvente", "Plasticos", "Decapante", "Espatula"}));
+        comboBox.setBounds(80, 95, 168, 22);
         comboBox.setForeground(new Color(255, 128, 64));
         comboBox.setFont(new Font("Tahoma", Font.PLAIN, 11));
-        comboBox.setBounds(80, 95, 168, 22);
         despegableProductos.add(comboBox);
 
         JLabel lblNewLabel_1 = new JLabel("Producto");
         lblNewLabel_1.setBounds(139, 67, 46, 14);
         despegableProductos.add(lblNewLabel_1);
 
-        JLabel precio = new JLabel("precio");
+        JLabel precio = new JLabel("Precio");
         precio.setBounds(276, 67, 46, 14);
         despegableProductos.add(precio);
 
         JButton btnNewButton = new JButton("Añadir");
-        btnNewButton.setBounds(507, 94, 105, 23);
+        btnNewButton.setBounds(490, 94, 122, 23);
         despegableProductos.add(btnNewButton);
 
-        JButton btnNewButton_1 = new JButton("eliminar");
-        btnNewButton_1.setBounds(622, 94, 109, 23);
+        JButton btnNewButton_1 = new JButton("Eliminar");
+        btnNewButton_1.setBounds(616, 94, 122, 23);
         despegableProductos.add(btnNewButton_1);
 
-        JButton btnNewButton_2 = new JButton("cambiar precio");
+        JButton btnNewButton_2 = new JButton("Cambiar precio");
         btnNewButton_2.setBounds(741, 94, 134, 23);
         despegableProductos.add(btnNewButton_2);
 
-        JSpinner spinner = new JSpinner();
-        spinner.setModel(new SpinnerNumberModel(Integer.valueOf(0), Integer.valueOf(0), null, Integer.valueOf(1)));
+        txtNuevoPrecio = new JTextField();
+        txtNuevoPrecio.setBounds(600, 130, 86, 20);
+        despegableProductos.add(txtNuevoPrecio);
+        txtNuevoPrecio.setColumns(10);
+
+        JLabel lblNuevoPrecio = new JLabel("Nuevo Precio:");
+        lblNuevoPrecio.setBounds(490, 130, 100, 20);
+        despegableProductos.add(lblNuevoPrecio);
+
+        spinner = new JSpinner(new SpinnerNumberModel(0, 0, null, 1));
         spinner.setBounds(432, 96, 48, 20);
         despegableProductos.add(spinner);
 
-        JLabel lblCantidad = new JLabel("cantidad");
-        lblCantidad.setBounds(351, 67, 46, 14);
-        despegableProductos.add(lblCantidad);
-
-        // Inicializa el precioDB que ya está declarado como atributo de la clase
         precioDB = new JLabel("");
+        precioDB.setBounds(275, 103, 46, 14);
         precioDB.setBackground(new Color(0, 255, 102));
-        precioDB.setForeground(new Color(204, 51, 102));
-        precioDB.setBounds(275, 103, 46, 14); // Ajusta el tamaño según sea necesario
+        precioDB.setForeground(new Color(204, 51, 153));
         despegableProductos.add(precioDB);
 
         cantidadDB = new JLabel("");
-        cantidadDB.setBackground(Color.GREEN);
         cantidadDB.setBounds(351, 103, 46, 14);
+        cantidadDB.setBackground(Color.GREEN);
         despegableProductos.add(cantidadDB);
 
-        comboBox.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (comboBox.getSelectedIndex() != 0) {
-                    String productoSeleccionado = comboBox.getSelectedItem().toString();
+        JLabel lblNewLabel = new JLabel(new ImageIcon(getClass().getResource("/resources/mancha arcoiris.jpg")));
+        lblNewLabel.setBounds(0, 0, 885, 698);
+        despegableProductos.add(lblNewLabel);
+
+        setupActionListeners(comboBox, btnNewButton, btnNewButton_1, btnNewButton_2);
+    }
+
+    private void setupActionListeners(JComboBox<String> comboBox, JButton btnAdd, JButton btnRemove, JButton btnChangePrice) {
+        comboBox.addActionListener(e -> {
+            if (comboBox.getSelectedIndex() != 0) {
+                String productoSeleccionado = comboBox.getSelectedItem().toString();
+                actualizarPrecio(productoSeleccionado);
+                txtNuevoPrecio.setText("");  // Limpiar el campo de nuevo precio
+            } else {
+                precioDB.setText(""); // Limpiar el texto si no se selecciona un producto
+            }
+        });
+
+        btnAdd.addActionListener(e -> {
+            int cantidad = (Integer) spinner.getValue();
+            String productoSeleccionado = comboBox.getSelectedItem().toString();
+            try {
+                AdministradorDB.añadirProducto(productoSeleccionado, cantidad);
+                actualizarPrecio(productoSeleccionado);
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al añadir producto: " + ex.getMessage());
+            }
+        });
+
+        btnRemove.addActionListener(e -> {
+            int cantidad = (Integer) spinner.getValue();
+            String productoSeleccionado = comboBox.getSelectedItem().toString();
+            try {
+                AdministradorDB.eliminarProducto(productoSeleccionado, cantidad);
+                actualizarPrecio(productoSeleccionado);
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al eliminar producto: " + ex.getMessage());
+            }
+        });
+
+        btnChangePrice.addActionListener(e -> {
+            String productoSeleccionado = comboBox.getSelectedItem().toString();
+            String inputPrecio = txtNuevoPrecio.getText();
+            if (inputPrecio != null && !inputPrecio.isEmpty()) {
+                try {
+                    double nuevoPrecio = Double.parseDouble(inputPrecio);
+                    AdministradorDB.actualizarPrecio(productoSeleccionado, nuevoPrecio);
                     actualizarPrecio(productoSeleccionado);
-                } else {
-                    precioDB.setText(""); // Limpiar el texto si no se selecciona un producto
+                    JOptionPane.showMessageDialog(null, "Precio actualizado correctamente.");
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "Por favor, ingrese un número válido.");
+                } catch (SQLException ex) {
+                    JOptionPane.showMessageDialog(null, "Error al actualizar precio: " + ex.getMessage());
                 }
+            } else {
+                JOptionPane.showMessageDialog(null, "Ingrese un precio.");
             }
         });
     }
@@ -102,14 +144,17 @@ public class Administrador extends JFrame {
         try {
             double precio = ProductoDB.obtenerPrecio(nombreProducto.toLowerCase());
             precioDB.setText(String.format("%.2f €", precio));
-            
-            double cant = ProductoDB.obtenerCantidad(nombreProducto);
-            cantidadDB.setText(String.format("%.2f ", cant));
-            
+            double cantidad = ProductoDB.obtenerCantidad(nombreProducto);
+            cantidadDB.setText(String.format("%.2f", cantidad));
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(this, "Error al obtener el precio: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
             precioDB.setText("Error");
         }
     }
 
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+           
+        });
+    }
 }
